fd66f074fcd100b74be82fc2c7af96a63ebd4110
Dan Nixon <dan@dan-nixon.com>
2014-10-31 18:13:20 +0000

    Add barebones GPS timelapse recorder

---
+++ 04-GPSTimelapseRecorder/gpstimelapse/__init__.py
@@ -0,0 +1,86 @@
+import argparse
+import logging
+import sys
+
+
+def run():
+    """
+    Gets command line options and starts timelapse recorder.
+    """
+    parser = argparse.ArgumentParser(description='GPS enabled timelapse recorder')
+
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='Increases console verbosity'
+    )
+
+    parser.add_argument(
+        '-g', '--gps',
+        action='store',
+        default='localhost:2947',
+        help='Specifies address to connect to cgps daemon (default localhost::2947)'
+    )
+
+    parser.add_argument(
+        '-f', '--folder',
+        action='store',
+        default='~/timelapse_recordings/',
+        help='Specifies folder to save timelapse recordings in'
+    )
+
+    parser.add_argument(
+        '-n', '--filename',
+        action='store',
+        default='%N_%T_%n.jpg',
+        help='Filename pattern for image files'
+    )
+
+    parser.add_argument(
+        '-d', '--distance',
+        action='store',
+        type=float,
+        help='Distance in meters to have moved between captures'
+    )
+
+    parser.add_argument(
+        '-i', '--interval',
+        action='store',
+        type=float,
+        help='Time in seconds between captures'
+    )
+
+    parser.add_argument(
+        '--log-file',
+        action='store',
+        default='gps_timelapse.log',
+        help='File to save log to'
+    )
+
+    parser.add_argument(
+        '--log-level',
+        action='store',
+        default='INFO',
+        help='Logging level [DEBUG,INFO,WARNING,ERROR,CRITICAL]'
+    )
+
+    props = parser.parse_args()
+
+    log_level = getattr(logging, props.log_level.upper(), None)
+    if not isinstance(log_level, int):
+        log_level = logging.INFO
+
+    logging.basicConfig(level=log_level, filename=props.log_file)
+    logging.getLogger(__name__).info('Got parameters, will start timelapse')
+
+    start_capture(props)
+
+
+def start_capture(props):
+    """
+    Runs the timelapse capture.
+
+    @param props Application properties
+    """
+
+    print props
---
+++ 04-GPSTimelapseRecorder/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='rpi-gps-timelapse',
+    version='0.1.0',
+    entry_points = {
+        'console_scripts': ['gpstimelapse=gpstimelapse:run'],
+    },
+    description='',
+    classifiers=[
+	'Natural Language :: English',
+	'Programming Language :: Python :: 2.7',
+    ],
+    url='http://github.com/DanNixon',
+    author='Dan Nixon',
+    author_email='dan@dan-nixon.com',
+    license='Apache',
+    packages=['gpstimelapse'],
+    install_requires=[
+        'enum34',
+    ],
+    include_package_data=True,
+    zip_safe=False)
