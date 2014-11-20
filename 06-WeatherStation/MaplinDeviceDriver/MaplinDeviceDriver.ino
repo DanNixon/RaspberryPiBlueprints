1736cd08c9d659759581dff57ae21cd5009477b6
Dan Nixon <dan@dan-nixon.com>
2014-11-20 19:49:10 +0000

    Added Arduino driver for Maplin weather instruments

---
+++ 06-WeatherStation/MaplinDeviceDriver/MaplinDeviceDriver.ino
@@ -0,0 +1,80 @@
+// Rain sensor between GND and digital pin 2
+// Wind speed sensor between GND and digital pin 3
+// Wind direction sensor between +5V and A0 with 10K pull down
+
+const uint8_t windSpeedHalfRevolutionsThreshold = 10;
+const uint16_t windDirectionLevels[] = {930, 830, 735, 390, 75, 135, 235, 560};
+const uint16_t windDirectionTolerance = 50;
+
+volatile bool rainFlag = false;
+volatile uint16_t windSpeedHalfRevolutions = 0;
+uint16_t windSpeedStartTime = 0;
+uint8_t lastWindDirection = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(0, rainTrigger, FALLING);
+
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(1, windSpeedTrigger, FALLING);
+
+  windSpeedStartTime = millis();
+}
+
+void loop()
+{
+  if(rainFlag)
+  {
+    Serial.println("RAIN_DETECT;");
+
+    rainFlag = false;
+  }
+
+  if(windSpeedHalfRevolutions > windSpeedHalfRevolutionsThreshold)
+  {
+    uint16_t deltaT = millis() - windSpeedStartTime;
+    float windSpeedRPM = 30000.0 / ((deltaT / (float)windSpeedHalfRevolutionsThreshold));
+    windSpeedStartTime = millis();
+    windSpeedHalfRevolutions = 0;
+
+    Serial.print("WIND_SPEED:RPM:");
+    Serial.print(windSpeedRPM);
+    Serial.println(";");
+  }
+
+  uint16_t wind_direction_reading = analogRead(0);
+  int8_t windDirection = -1;
+  for(int i = 0; i < 8; i++)
+  {
+    uint16_t rangeMin = windDirectionLevels[i] - windDirectionTolerance;
+    uint16_t rangeMax = windDirectionLevels[i] + windDirectionTolerance;
+
+    if(rangeMin < wind_direction_reading && rangeMax > wind_direction_reading)
+    {
+      windDirection = i;
+      break;
+    }
+  }
+
+  if(lastWindDirection != windDirection && windDirection != -1)
+  {
+    lastWindDirection = windDirection;
+
+    Serial.print("WIND_DIRECTION:ARB:");
+    Serial.print(windDirection);
+    Serial.println(";");
+  }
+}
+
+void rainTrigger()
+{
+  rainFlag = true;
+}
+
+void windSpeedTrigger()
+{
+  windSpeedHalfRevolutions++;
+}
