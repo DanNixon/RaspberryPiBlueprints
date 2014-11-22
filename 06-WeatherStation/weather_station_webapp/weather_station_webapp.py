801617e796c0bcf952294f47d2fba7485f14c85e
Dan Nixon <dan@dan-nixon.com>
2014-11-22 11:26:56 +0000

    Added time range to history page

--- 06-WeatherStation/weather_station_webapp/weather_station_webapp.py
+++ 06-WeatherStation/weather_station_webapp/weather_station_webapp.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
-import os
+"""
+Basic web application to view data collected by the weather station.
+"""
+
+import os, time
+from datetime import timedelta
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
                   render_template, flash
 
-# create our little application :)
 app = Flask(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'weather.db'),
-    DEBUG=True,
-    SECRET_KEY='development key'
+    DEBUG=True
 ))
+
 app.config.from_envvar('WEATHER_SETTINGS', silent=True)
 
 
@@ -73,7 +77,31 @@ def show_current():
 
 @app.route('/history')
 def show_history():
+    # Get the requested time range
+    requested_from = request.args.get('from_time')
+    requested_to = request.args.get('to_time')
+
+    # The format requests from the timestamp window form come in
+    request_format = '%Y-%m-%dT%H:%M'
+
+    # Get the end of the time range (defaults to now)
+    timestamp_to = int(time.time())
+    if requested_to is not None and not requested_to == '':
+        timestamp_to = time.mktime(time.strptime(requested_to, request_format))
+    else:
+        requested_to = time.strftime(request_format, time.gmtime(timestamp_to))
+
+    # Get the start of the time range (defaults to a week before end)
+    timestamp_from = timestamp_to - int(timedelta(days=7).total_seconds())
+    if requested_from is not None and not requested_from == '':
+        timestamp_from = time.mktime(time.strptime(requested_from, request_format))
+    else:
+        requested_from = time.strftime(request_format, time.gmtime(timestamp_from))
+
     db = get_db()
-    cur = db.execute('SELECT wind_direction, wind_speed FROM weather_history ORDER BY timestamp DESC')
+    cur = db.execute("SELECT * FROM weather_history WHERE timestamp <= DATETIME(%d, 'unixepoch') AND timestamp > DATETIME(%d, 'unixepoch') ORDER BY timestamp DESC"
+            % (timestamp_to, timestamp_from))
+
     data = cur.fetchall()
-    return render_template('show_history.html', data=data)
+
+    return render_template('show_history.html', data=data, time_to=requested_to, time_from=requested_from)
