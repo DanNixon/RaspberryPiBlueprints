a1aaed08800d27e0c317de9657a464968f470fc1
Dan Nixon <dan@dan-nixon.com>
2014-11-22 15:57:07 +0000

    Add history page

--- 06-WeatherStation/weather_station_webapp/weather_station_webapp.py
+++ 06-WeatherStation/weather_station_webapp/weather_station_webapp.py
@@ -3,7 +3,7 @@
 Basic web application to view data collected by the weather station.
 """
 
-import os, time
+import os, time, json
 from datetime import timedelta
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
@@ -75,8 +75,58 @@ def show_current():
     return render_template('show_current.html', data=data)
 
 
+def get_peak_wind_speed(time_from, time_to):
+    """
+    Gets the peak wind speed recorded in a given time.
+
+    @param time_from Start time
+    @param time_to End time
+    @return Peak wind speed over the time range
+    """
+
+    # Get data from database
+    db = get_db()
+    db.row_factory = list_factory
+    cur = db.execute("SELECT peak_wind_speed FROM weather_history WHERE timestamp <= DATETIME(%d, 'unixepoch') AND timestamp > DATETIME(%d, 'unixepoch') ORDER BY timestamp DESC"
+            % (time_to, time_from))
+    rows = cur.fetchall()
+
+    # Find the max wind speed for this time
+    max_wind_speed = 0;
+    for point in rows:
+        if point[1] > max_wind_speed:
+            max_wind_speed = point[1]
+
+    return max_wind_speed
+
+
+def list_factory(cursor, row):
+    """
+    Used to format SQLite rows as lists
+
+    @param cursor Current sursor
+    @param row Current row
+    @return Row as a list
+    """
+
+    d = list()
+    for idx, col in enumerate(cursor.description):
+        d.insert(idx, row[idx])
+    return d
+
+
 @app.route('/history')
 def show_history():
+    # Get the columns to be retrieved from the database
+    requested_columns = request.args.getlist('data_items')
+    if len(requested_columns) > 0:
+        columns = requested_columns
+    else:
+        columns = ['temperature', 'pressure']
+
+    # Always need the first column to be timestamp
+    columns.insert(0, 'timestamp')
+
     # Get the requested time range
     requested_from = request.args.get('from_time')
     requested_to = request.args.get('to_time')
@@ -100,16 +150,34 @@ def show_history():
 
     # Get data from database
     db = get_db()
-    cur = db.execute("SELECT * FROM weather_history WHERE timestamp <= DATETIME(%d, 'unixepoch') AND timestamp > DATETIME(%d, 'unixepoch') ORDER BY timestamp DESC"
-            % (timestamp_to, timestamp_from))
-    data = cur.fetchall()
+    db.row_factory = list_factory
+    cur = db.execute("SELECT %s FROM weather_history WHERE timestamp <= DATETIME(%d, 'unixepoch') AND timestamp > DATETIME(%d, 'unixepoch') ORDER BY timestamp DESC"
+            % (','.join(columns), timestamp_to, timestamp_from))
+    rows = cur.fetchall()
 
-    # Find the max wind speed for this time
-    max_wind_speed = 0;
-    for point in data:
-        if point['peak_wind_speed'] > max_wind_speed:
-            max_wind_speed = point['peak_wind_speed']
+    column_name_to_label = {
+                'timestamp' : 'Timestamp',
+                'temperature' : 'Temperature',
+                'humidity' : 'Humidity',
+                'pressure' : 'Pressure',
+                'average_wind_speed' : 'Average Wind Speed',
+                'peak_wind_speed' : 'Peak Wind Speed',
+                'light_level' : 'Light Level',
+                'rain_frequency' : 'Rain'
+            }
 
-    return render_template('show_history.html', data=data,
+    # Get friendly column names
+    labels = [column_name_to_label[col] for col in columns]
+
+    # Convert the data into a nice format for the template
+    data = list()
+    for point in rows:
+        data.append([point[0], ','.join([str(val) for i, val in enumerate(point) if i > 0])])
+
+    # Get max wind speed over the time range
+    max_wind_speed = get_peak_wind_speed(timestamp_to, timestamp_from)
+
+    return render_template('show_history.html', data=data, labels=labels,
+                           peak_wind_speed=max_wind_speed,
                            time_to=requested_to, time_from=requested_from,
-                           peak_wind_speed=max_wind_speed)
+                           selected_columns=columns)
