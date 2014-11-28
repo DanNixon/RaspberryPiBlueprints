50bf841de585226cbe3c6ea22c717125d89eff67
Dan Nixon <dan@dan-nixon.com>
2014-11-28 19:36:31 +0000

    More refactoring, added most methods

--- 07-SecuritySystem/security_webapp/security.py
+++ 07-SecuritySystem/security_webapp/security.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-    Flaskr
-    ~~~~~~
-
-    A microblog example application written as Flask tutorial with
-    Flask and sqlite3.
-
-    :copyright: (c) 2014 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
 """
 
 import os
@@ -16,12 +8,11 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 
-# create our little application :)
 app = Flask(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DATABASE=os.path.join(app.root_path, 'security.db'),
     DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
@@ -31,14 +22,18 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def connect_db():
-    """Connects to the specific database."""
+    """
+    Connects to the specific database.
+    """
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
 
 
 def init_db():
-    """Initializes the database."""
+    """
+    Initializes the database.
+    """
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
@@ -47,13 +42,16 @@ def init_db():
 
 @app.cli.command('initdb')
 def initdb_command():
-    """Creates the database tables."""
+    """
+    Creates the database tables.
+    """
     init_db()
     print('Initialized the database.')
 
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
+    """
+    Opens a new database connection if there is none yet for the
     current application context.
     """
     if not hasattr(g, 'sqlite_db'):
@@ -63,29 +61,131 @@ def get_db():
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the database again at the end of the request."""
+    """
+    Closes the database again at the end of the request.
+    """
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
 
 @app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+def show_home():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
+    return redirect(url_for('show_sensors'))
 
 
-@app.route('/add', methods=['POST'])
-def add_entry():
+@app.route('/sensors')
+def show_sensors():
     if not session.get('logged_in'):
         abort(401)
+
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    cur = db.execute('SELECT * FROM sensors ORDER BY id ASC')
+    sensors = cur.fetchall()
+    return render_template('show_sensors.html', sensors=sensors)
+
+
+@app.route('/sensors/<sensor_id>/delete')
+def delete_sensor(sensor_id):
+    if not session.get('logged_in'):
+        abort(401)
+
+    db = get_db()
+    cur = db.execute('DELETE FROM sensors WHERE id = %d' % sensor_id)
+
+    # TODO
+
+    return redirect(url_for('show_sensors'))
+
+
+@app.route('/sensors/<sensor_id>/update')
+def update_sensor(sensor_id):
+    if not session.get('logged_in'):
+        abort(401)
+
+    # TODO
+
+    return redirect(url_for('show_sensors'))
+
+
+@app.route('/sensors/add')
+def add_sensor():
+    if not session.get('logged_in'):
+        abort(401)
+
+    # TODO
+
+    return redirect(url_for('show_sensors'))
+
+
+@app.route('/events')
+def show_events():
+    if not session.get('logged_in'):
+        abort(401)
+
+    db = get_db()
+    cur = db.execute('SELECT * FROM events ORDER BY timestamp DESC')
+    events = cur.fetchall()
+    return render_template('show_events.html', events=events)
+
+
+@app.route('/events/<event_id>/delete')
+def delete_event(event_id):
+    if not session.get('logged_in'):
+        abort(401)
+
+    db = get_db()
+    cur = db.execute('DELETE FROM events WHERE id = %d' % event_id)
+
+    # TODO
+
+    return redirect(url_for('show_events'))
+
+
+@app.route('/alarms')
+def show_alarms():
+    if not session.get('logged_in'):
+        abort(401)
+
+    db = get_db()
+    cur = db.execute('SELECT * FROM alarms ORDER BY id ASC')
+    alarms = cur.fetchall()
+    return render_template('show_alarms.html', alarms=alarms)
+
+
+@app.route('/alarms/<alarm_id>/delete')
+def delete_alarm(alarm_id):
+    if not session.get('logged_in'):
+        abort(401)
+
+    db = get_db()
+    cur = db.execute('DELETE FROM alarms WHERE id = %d' % alarm_id)
+
+    # TODO
+
+    return redirect(url_for('show_alarms'))
+
+
+@app.route('/alarms/<alarm_id>/update')
+def update_alarm(alarm_id):
+    if not session.get('logged_in'):
+        abort(401)
+
+    # TODO
+
+    return redirect(url_for('show_alarms'))
+
+
+@app.route('/alarms/add')
+def add_alarm():
+    if not session.get('logged_in'):
+        abort(401)
+
+    # TODO
+
+    return redirect(url_for('show_alarms'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -99,7 +199,7 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            return redirect(url_for('show_entries'))
+            return redirect(url_for('show_home'))
     return render_template('login.html', error=error)
 
 
@@ -107,4 +207,4 @@ def login():
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_home'))
