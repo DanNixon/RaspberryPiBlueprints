e92b4c402875975f1966a6baacd02e8c9ec18370
Dan Nixon <dan@dan-nixon.com>
2014-12-07 10:00:08 +0000

    Document RF receiver

--- 07-SecuritySystem/rf_network/BaseNode_Arduino/BaseNode_Arduino.ino
+++ 07-SecuritySystem/rf_network/BaseNode_Arduino/BaseNode_Arduino.ino
@@ -4,12 +4,18 @@
 
 #define SERIAL_BAUD 115200
 
+// RF drivers
 RF24 radio(9, 10);
 RF24Network network(radio);
 
+// RF channel, change this to get best coverage and reliability
 const uint16_t CHANNEL = 90;
-const uint16_t THIS_NODE_ADDR = 00;
+// Base node address, do not change this
+const uint16_t BASE_NODE_ADDR = 00;
 
+/**
+ * The data that is sent in a single RF message.
+ */
 struct payload_t
 {
   char topic[10];
@@ -18,25 +24,32 @@ struct payload_t
 
 void setup(void)
 {
+  // Start the serial port
   Serial.begin(SERIAL_BAUD);
 
+  // Start the RF node and network
   SPI.begin();
   radio.begin();
-  network.begin(CHANNEL, THIS_NODE_ADDR);
+  network.begin(CHANNEL, BASE_NODE_ADDR);
 }
 
 void loop(void)
 {
+  // Pump the network
   network.update();
 
+  // Check for new messages
   while(network.available())
   {
+    // Get the new message
     RF24NetworkHeader header;
     payload_t payload;
-    network.read(header,&payload,sizeof(payload));
+    network.read(header, &payload, sizeof(payload));
 
+    // Print it to serial port
     Serial.print(payload.topic);
     Serial.print(":");
-    Serial.println(payload.state);
+    Serial.print(payload.state);
+    Serial.println(";");
   }
 }
