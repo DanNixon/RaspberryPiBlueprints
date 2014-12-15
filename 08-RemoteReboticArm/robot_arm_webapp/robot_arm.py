f6685c6ebe94d4d0dc323608f10ded5b2f377006
Dan Nixon <dan@dan-nixon.com>
2014-12-15 21:15:07 +0000

    Use relay h bridge for motors

--- 08-RemoteReboticArm/robot_arm_webapp/robot_arm.py
+++ 08-RemoteReboticArm/robot_arm_webapp/robot_arm.py
@@ -6,6 +6,7 @@ Web application to control servos for a robot arm via RPi GPIO.
 import os, logging
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template
+import RPIO
 from RPIO import PWM
 
 
@@ -18,25 +19,28 @@ app.config.update(dict(
 
     SERVO_DELTA=50,
 
-    LEFT_C_SERVO_GPIO=None,
-    RIGHT_C_SERVO_GPIO=None,
-    ARM_1_SERVO_GPIO=None,
+    ARM_1_A_SERVO_GPIO=None,
+    ARM_1_B_SERVO_GPIO=None,
     ARM_2_SERVO_GPIO=None,
     GRIP_SERVO_GPIO=None,
 
-    LEFT_C_SERVO_MIN=600,
-    RIGHT_C_SERVO_MIN=600,
-    ARM_1_SERVO_MIN=600,
+    ARM_1_A_SERVO_MIN=600,
+    ARM_1_B_SERVO_MIN=600,
     ARM_2_SERVO_MIN=600,
     GRIP_SERVO_MIN=600,
 
-    LEFT_C_SERVO_MAX=2500,
-    RIGHT_C_SERVO_MAX=2500,
-    ARM_1_SERVO_MAX=2500,
+    ARM_1_A_SERVO_MAX=2500,
+    ARM_1_B_SERVO_MAX=2500,
     ARM_2_SERVO_MAX=2500,
-    GRIP_SERVO_MAX=2500
+    GRIP_SERVO_MAX=2500,
+
+    MOTOR_POWER_GPIO=None,
+    MOTOR_1_A_GPIO=None,
+    MOTOR_1_B_GPIO=None,
+    MOTOR_2_A_GPIO=None,
+    MOTOR_2_B_GPIO=None
 ))
-app.config.from_envvar('ROBOT_ARM_SETTINGS', silent=True)
+app.config.from_envvar('ROBOT_ARM_SETTINGS', silent=False)
 
 
 log_level = getattr(logging, app.config['LOG_LEVEL'].upper(), None)
@@ -44,6 +48,12 @@ logging.basicConfig(level=log_level, filename=app.config['LOG_FILE'])
 
 SERVO_POSITIONS = dict()
 SERVOS = dict()
+
+RPIO.setup(app.config['MOTOR_1_A_GPIO'], RPIO.OUT, initial=RPIO.HIGH)
+RPIO.setup(app.config['MOTOR_1_B_GPIO'], RPIO.OUT, initial=RPIO.HIGH)
+RPIO.setup(app.config['MOTOR_2_A_GPIO'], RPIO.OUT, initial=RPIO.HIGH)
+RPIO.setup(app.config['MOTOR_2_B_GPIO'], RPIO.OUT, initial=RPIO.HIGH)
+RPIO.setup(app.config['MOTOR_POWER_GPIO'], RPIO.OUT, initial=RPIO.HIGH)
 
 
 def set_servo(servo_id, position):
@@ -76,15 +86,53 @@ def set_servo(servo_id, position):
     SERVO_POSITIONS[servo_id] = time_val
 
 
+def set_base_movement(movement_type):
+    """
+    Sets a base movement type on the motor outputs.
+
+    @param movement_type Movement command.
+    """
+
+    if movement_type == 'mov_fwd':
+        RPIO.output(app.config['MOTOR_1_A_GPIO'], False)
+        RPIO.output(app.config['MOTOR_1_B_GPIO'], True)
+        RPIO.output(app.config['MOTOR_2_A_GPIO'], False)
+        RPIO.output(app.config['MOTOR_2_B_GPIO'], True)
+    elif movement_type == 'mov_rev':
+        RPIO.output(app.config['MOTOR_1_A_GPIO'], True)
+        RPIO.output(app.config['MOTOR_1_B_GPIO'], False)
+        RPIO.output(app.config['MOTOR_2_A_GPIO'], True)
+        RPIO.output(app.config['MOTOR_2_B_GPIO'], False)
+    elif movement_type == 'rot_cw':
+        RPIO.output(app.config['MOTOR_1_A_GPIO'], False)
+        RPIO.output(app.config['MOTOR_1_B_GPIO'], True)
+        RPIO.output(app.config['MOTOR_2_A_GPIO'], True)
+        RPIO.output(app.config['MOTOR_2_B_GPIO'], False)
+    elif movement_type == 'rot_ccw':
+        RPIO.output(app.config['MOTOR_1_A_GPIO'], True)
+        RPIO.output(app.config['MOTOR_1_B_GPIO'], False)
+        RPIO.output(app.config['MOTOR_2_A_GPIO'], False)
+        RPIO.output(app.config['MOTOR_2_B_GPIO'], True)
+    elif movement_type == 'stop':
+        pass
+    else:
+        logging.getLogger(__name__).error('Unknown movement type: %s' % movement_type)
+
+    power = movement_type != 'stop'
+    logging.getLogger(__name__).debug('Setting motor power: %s' % str(power))
+    RPIO.output(app.config['MOTOR_POWER_GPIO'], not power)
+
+
 def set_servos_default():
     """
     Sets all servos to their default position.
     """
     logging.getLogger(__name__).info('Resetting servos to default position')
-
-    all_servo_ids = ['LEFT_C_SERVO', 'RIGHT_C_SERVO', 'ARM_1_SERVO', 'ARM_2_SERVO', 'GRIP_SERVO']
+    all_servo_ids = ['ARM_1_A_SERVO', 'ARM_1_B_SERVO', 'ARM_2_SERVO', 'GRIP_SERVO']
     for servo_id in all_servo_ids:
         set_servo(servo_id, app.config[servo_id + '_MIN'])
+
+    logging.getLogger(__name__).info('Enabling motor power')
 
 
 @app.route('/')
@@ -104,22 +152,24 @@ def show_video():
 
 @app.route('/command/<command>')
 def handle_command(command):
+    """
+    Handles a command from the web UI.
+
+    @param command Command sent to web API
+    """
+
     logging.getLogger(__name__).debug('Got command: %s' % command)
 
-    if command == 'mov_fwd':
-        pass
-    elif command == 'mov_rev':
-        pass
-    elif command == 'rot_cw':
-        pass
-    elif command == 'rot_ccw':
-        pass
-    elif command == 'stop':
-        pass
+    base_movement_commands = ['mov_fwd', 'mov_rev', 'rot_cw', 'rot_ccw', 'stop']
+
+    if command in base_movement_commands:
+        set_base_movement(command)
     elif command == 'arm1_raise':
-        set_servo('ARM_1_SERVO', SERVO_POSITIONS['ARM_1_SERVO'] + app.config['SERVO_DELTA'])
+        set_servo('ARM_1_A_SERVO', SERVO_POSITIONS['ARM_1_A_SERVO'] + app.config['SERVO_DELTA'])
+        set_servo('ARM_1_B_SERVO', SERVO_POSITIONS['ARM_1_B_SERVO'] + app.config['SERVO_DELTA'])
     elif command == 'arm1_lower':
-        set_servo('ARM_1_SERVO', SERVO_POSITIONS['ARM_1_SERVO'] - app.config['SERVO_DELTA'])
+        set_servo('ARM_1_A_SERVO', SERVO_POSITIONS['ARM_1_A_SERVO'] - app.config['SERVO_DELTA'])
+        set_servo('ARM_1_B_SERVO', SERVO_POSITIONS['ARM_1_B_SERVO'] - app.config['SERVO_DELTA'])
     elif command == 'arm2_raise':
         set_servo('ARM_2_SERVO', SERVO_POSITIONS['ARM_2_SERVO'] - app.config['SERVO_DELTA'])
     elif command == 'arm2_lower':
@@ -134,5 +184,6 @@ def handle_command(command):
     return redirect(url_for('show_control'))
 
 
-# Set servos to default position when app starts
+# Set servos to default position and base motors when app starts
 set_servos_default()
+set_base_movement('stop')
