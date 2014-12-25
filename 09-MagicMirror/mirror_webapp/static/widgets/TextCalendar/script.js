0b74997cf3ac3cfff63bfb5c4ad083443d338f7b
Dan Nixon <dan@dan-nixon.com>
2014-12-25 19:23:05 +0000

    Initial code for new calendar widget

---
+++ 09-MagicMirror/mirror_webapp/static/widgets/TextCalendar/script.js
@@ -0,0 +1,10 @@
+var TextCalendar = function() {
+}
+
+TextCalendar.prototype.init = function(widgetDOM) {
+  return;
+};
+
+TextCalendar.prototype.update = function(widgetDOM) {
+  return;
+};
---
+++ 09-MagicMirror/mirror_webapp/widgets/TextCalendar.py
@@ -0,0 +1,5 @@
+from AbstractClock import AbstractClock
+
+
+class TextCalendar(AbstractClock):
+    pass
