b5ef5f1c5f3785174407c4f7d8acab9e69944c2a
Dan Nixon <dan@dan-nixon.com>
2014-12-25 19:12:51 +0000

    Add more info to clock webservice

--- 09-MagicMirror/mirror_webapp/widgets/AbstractClock.py
+++ 09-MagicMirror/mirror_webapp/widgets/AbstractClock.py
@@ -2,26 +2,32 @@ from AbstractWidget import AbstractWidget
 from ConfigParser import NoOptionError
 from pytz import timezone
 from datetime import datetime
+from calendar import timegm
 
 
 class AbstractClock(AbstractWidget):
 
     def get_data(self, config):
+        data = dict()
+
         try:
-            user_timesone = config.get('widget', 'timezone')
-            time_zone = timezone(user_timesone)
+            user_timezone = config.get('widget', 'timezone')
+            time_zone = timezone(user_timezone)
+            data['timezone'] = time_zone.zone
             self.logger.info('Get timezone form config file: %s' % str(time_zone))
             current_time = datetime.now(time_zone)
 
         except NoOptionError:
+            data['timezone'] = 'System Time'
             current_time = datetime.now()
 
-        data = dict()
         data['year'] = current_time.year
         data['month'] = current_time.month
         data['day'] = current_time.day
         data['hour'] = current_time.hour
         data['minute'] = current_time.minute
         data['second'] = current_time.second
+
+        data['epoch_seconds'] = timegm(current_time.utctimetuple())
 
         return data
