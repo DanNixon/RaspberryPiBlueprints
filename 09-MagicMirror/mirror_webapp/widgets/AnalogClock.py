cee635fef9739b43605729a7fb74a974c737af2c
Dan Nixon <dan@dan-nixon.com>
2014-12-25 19:07:21 +0000

    Made clock abstract

--- 09-MagicMirror/mirror_webapp/widgets/AnalogClock.py
+++ 09-MagicMirror/mirror_webapp/widgets/AnalogClock.py
@@ -1,27 +1,5 @@
-from AbstractWidget import AbstractWidget
-from ConfigParser import NoOptionError
-from pytz import timezone
-from datetime import datetime
+from AbstractClock import AbstractClock
 
 
-class AnalogClock(AbstractWidget):
-
-    def get_data(self, config):
-        try:
-            user_timesone = config.get('widget', 'timezone')
-            time_zone = timezone(user_timesone)
-            self.logger.info('Get timezone form config file: %s' % str(time_zone))
-            current_time = datetime.now(time_zone)
-
-        except NoOptionError:
-            current_time = datetime.now()
-
-        data = dict()
-        data['year'] = current_time.year
-        data['month'] = current_time.month
-        data['day'] = current_time.day
-        data['hour'] = current_time.hour
-        data['minute'] = current_time.minute
-        data['second'] = current_time.second
-
-        return data
+class AnalogClock(AbstractClock):
+    pass
