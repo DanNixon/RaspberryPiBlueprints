4aa04dbca43f2b394abc8b84811c94b24975553e
Dan Nixon <dan@dan-nixon.com>
2014-12-27 16:13:20 +0000

    Added news summary widget

--- 09-MagicMirror/mirror_webapp/static/widgets/RSSFeed/script.js
+++ 09-MagicMirror/mirror_webapp/static/widgets/RSSFeed/script.js
@@ -1,10 +1,24 @@
 var RSSFeed = function() {
+  this.updateFeed = function(widgetDOM) {
+    feedList = widgetDOM.getElementsByClassName('rss-items')[0];
+    feedList.innerHTML = '';
+
+    $.getJSON('/widget_data/' + widgetDOM.id, function(data) {
+      var items = data.items;
+      for(i = 0; i < items.length; i++) {
+        var newItem = document.createElement('li');
+        var itemTitle = document.createTextNode(items[i].title);
+        newItem.appendChild(itemTitle);
+        feedList.appendChild(newItem);
+      }
+    });
+  };
 }
 
 RSSFeed.prototype.init = function(widgetDOM) {
-  return;
+  this.updateFeed(widgetDOM);
 };
 
 RSSFeed.prototype.update = function(widgetDOM) {
-  return;
+  this.updateFeed(widgetDOM);
 };
