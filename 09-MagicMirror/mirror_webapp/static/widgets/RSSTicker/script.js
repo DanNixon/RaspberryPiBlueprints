5991b133b712c92d89723f7bc928d6b9bcbf1f51
Dan Nixon <dan@dan-nixon.com>
2014-12-27 20:16:27 +0000

    Added RSS ticker

--- 09-MagicMirror/mirror_webapp/static/widgets/RSSTicker/script.js
+++ 09-MagicMirror/mirror_webapp/static/widgets/RSSTicker/script.js
@@ -1,10 +1,44 @@
 var RSSTicker = function() {
+  var that = this;
+
+  var items = [];
+  var currentItemIndex = 0;
+  var interval;
+  var widgetDOM;
+
+  this.updateItemsList = function() {
+    $.getJSON('/widget_data/' + this.widgetDOM.id, function(data) {
+      items = data.items;
+
+      clearInterval(interval);
+      interval = setInterval(that.newItem, data.update_time * 1000);
+
+      that.newItem();
+    });
+  };
+
+  this.newItem = function() {
+    var itemText = items[currentItemIndex];
+
+    var widgetTextDOM = that.widgetDOM.getElementsByClassName('rss-ticker-item')[0];
+    $(widgetTextDOM).fadeOut(400, function() {
+      widgetTextDOM.innerText = itemText;
+      $(widgetTextDOM).fadeIn(400);
+    });
+
+    currentItemIndex += 1;
+
+    if(currentItemIndex == items.length) {
+      currentItemIndex = 0;
+    }
+  };
 }
 
 RSSTicker.prototype.init = function(widgetDOM) {
-  return;
+  this.widgetDOM = widgetDOM;
+  this.updateItemsList();
 };
 
 RSSTicker.prototype.update = function(widgetDOM) {
-  return;
+  this.updateItemsList();
 };
--- 09-MagicMirror/mirror_webapp/widgets/RSSTicker.py
+++ 09-MagicMirror/mirror_webapp/widgets/RSSTicker.py
@@ -5,4 +5,11 @@ class RSSTicker(AbstractRSSFeed):
 
     def get_data(self, config):
         data = AbstractRSSFeed.get_data(self, config)
+
+        text_type = config.get('text_type', 'title')
+        data['update_time'] = config.get('ticker_update_time', 5)
+
+        new_items = [item[text_type] for item in data['items']]
+        data['items'] = new_items
+
         return data
