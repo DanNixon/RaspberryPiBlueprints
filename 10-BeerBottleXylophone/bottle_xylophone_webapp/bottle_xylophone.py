efe2da95723f7e87df7e6293da2ea48392625d55
Dan Nixon <dan@dan-nixon.com>
2015-01-07 06:45:28 +0000

    Basic layout, got file uploads working

--- 10-BeerBottleXylophone/bottle_xylophone_webapp/bottle_xylophone.py
+++ 10-BeerBottleXylophone/bottle_xylophone_webapp/bottle_xylophone.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug import secure_filename
 
 
 app = Flask(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DEBUG=True
+    DEBUG=True,
+    SECRET_KEY='key',
+    MIDI_DIRECTORY='/home/dan/midi_temp'
 ))
 app.config.from_envvar('BOTTLE_XYLOPHONE_SETTINGS', silent=True)
 
@@ -15,3 +19,15 @@ app.config.from_envvar('BOTTLE_XYLOPHONE_SETTINGS', silent=True)
 @app.route('/')
 def show_home():
     return render_template('home.html')
+
+
+@app.route('/upload_midi', methods=['POST'])
+def upload_midi():
+    file = request.files['uploaded_midi_file']
+
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['MIDI_DIRECTORY'], filename))
+
+        flash('Uploaded MIDI file: %s' % filename)
+        return redirect(url_for('show_home'))
